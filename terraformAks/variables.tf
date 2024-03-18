fa048493d444cc01e802777a13d364f74527358e
Shashi Ranjan <shashi11107@gmail.com>
2024-03-18 15:34:08 +0530

    Updating the node count

--- terraformAks/variables.tf
+++ terraformAks/variables.tf
@@ -13,17 +13,5 @@ variable "resource_group_name_prefix" {
 variable "node_count" {
   type        = number
   description = "The initial quantity of nodes for the node pool."
-  default     = 1
-}
-
-variable "msi_id" {
-  type        = string
-  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
-  default     = null
-}
-
-variable "username" {
-  type        = string
-  description = "The admin username for the new cluster."
-  default     = "azureadmin"
+  default     = 2
 }
